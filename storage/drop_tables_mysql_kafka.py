7b766cb8690ef99effe2ca86e744b767cdd584dc
HenrikTDang <62926814+HenrikTDang@users.noreply.github.com>
2021-11-02 23:35:57 -0700

    Update drop_tables_mysql_kafka.py

--- storage/drop_tables_mysql_kafka.py
+++ storage/drop_tables_mysql_kafka.py
@@ -1,6 +1,6 @@
 import mysql.connector 
 
-db_conn = mysql.connector.connect(host="microservice-henrik.eastus.cloudapp.azure.com", user="root", 
+db_conn = mysql.connector.connect(host="microservice-henrik.eastus.cloudapp.azure.com", user="user", 
 password="Password", database="events") 
 db_cursor = db_conn.cursor()
 db_cursor.execute(''' DROP TABLE online_sales, instore_sales ''') 
