9956d728ec192005531b52257b2a659b8bc31bb7
HenrikTDang <henrikt.dang@gmail.com>
2021-11-07 20:36:18 -0800

    update ui to audit endpoint

--- dashboard-ui/src/App.js
+++ dashboard-ui/src/App.js
@@ -6,7 +6,7 @@ import AppStats from './components/AppStats'
 
 function App() {
 
-    const endpoints = ["instore_sales", "online_sales"]
+    const endpoints = ["/sales/instore", "/sales/online"]
 
     const rendered_endpoints = endpoints.map((endpoint) => {
         return <EndpointAudit key={endpoint} endpoint={endpoint}/>
